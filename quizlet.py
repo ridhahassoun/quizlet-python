79fdca35fd4fc1e9bee795c26330b5b1568d081a
Ridha Hassoun <ridha.hassoun@gmail.com>
2016-08-04 11:45:07 -0400

    Wrote QuizletSession class to run through OAuth2 flow. Fixed some issues with _QuizletAuthorizationHTTPHandler regarding checking state.

--- quizlet.py
+++ quizlet.py
@@ -4,9 +4,21 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib import parse
 from uuid import uuid4
 import requests
+import webbrowser
 
 class QuizletSession():
     """docstring for QuizletSession"""
+    def __init__(self, scope="read"):
+        state = str(uuid4()) # generate a string to send and receive to verify and prevent CSRF attacks
+        print(state)
+
+        url = _make_authorization_url(state, scope)
+        webbrowser.open_new(url)
+
+        server = HTTPServer(('', 8000), _QuizletAuthorizationHTTPHandler)
+        server.sent_state = state
+
+        server.handle_request()
 
 class _QuizletAuthorizationHTTPHandler(BaseHTTPRequestHandler):
     
@@ -16,21 +28,26 @@ class _QuizletAuthorizationHTTPHandler(BaseHTTPRequestHandler):
 
     This class should not be called directly."""
 
-    def do_GET():
+    def do_GET(self):
         # Set up the web page for the redirect.
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
 
-        self.wfile.write("You may close the window now.", "UTF-8")
+        self.wfile.write(bytes("You may close the window now.", "UTF-8"))
 
         # Check to see if user allowed access and Quizlet returned an authorization code
         if "code" in self.path:
-            queries   = parse.urlparse(self.path).query
-            auth_code = parse.parse_qs(queries)["code"][0]
+            queries = parse.urlparse(self.path).query
+            
+            # make sure the state we sent is the same as the state returned
+            if parse.parse_qs(queries)["state"][0] == self.server.sent_state: 
+                auth_code = parse.parse_qs(queries)["code"][0]
+
+                access_token = _get_access_token(auth_code)
+                print(access_token)
 
 def _make_authorization_url(state, scope):
-    #state = str(uuid4()) # generate a string to send and receive to verify and prevent CSRF attacks
     params = {"client_id": CLIENT_ID,
               "response_type": "code",
               "state": state,
@@ -45,9 +62,8 @@ def _get_access_token(authorization_code):
     client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
     post_data = {"grant_type": "authorization_code",
                  "code": authorization_code,
-                 "redirect_uri": "http://localhost:8000"}
-
+                 "redirect_uri": "http://localhost:8000/"
+    }
     response = requests.post("https://api.quizlet.com/oauth/token", auth=client_auth, data=post_data)
     token_json = response.json()
-    
     return token_json
