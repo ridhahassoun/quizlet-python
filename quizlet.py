993fd720552052f00b939e96a2fcfc915cfe514a
Ridha Hassoun <ridha.hassoun@gmail.com>
2016-08-04 10:49:02 -0400

    Wrote _make_authorization_url and _get_access_token functions to generate authentication URL and request access token.

--- quizlet.py
+++ quizlet.py
@@ -2,11 +2,20 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib import parse
+from uuid import uuid4
+import requests
 
 class QuizletSession():
     """docstring for QuizletSession"""
 
 class _QuizletAuthorizationHTTPHandler(BaseHTTPRequestHandler):
+    
+    """A subclass of BaseHTTPRequestHandler that is used internally  
+    to help manage the redirect from Quizlet and complete the
+    authentication process.
+
+    This class should not be called directly."""
+
     def do_GET():
         # Set up the web page for the redirect.
         self.send_response(200)
@@ -19,3 +28,26 @@ class _QuizletAuthorizationHTTPHandler(BaseHTTPRequestHandler):
         if "code" in self.path:
             queries   = parse.urlparse(self.path).query
             auth_code = parse.parse_qs(queries)["code"][0]
+
+def _make_authorization_url(state, scope):
+    #state = str(uuid4()) # generate a string to send and receive to verify and prevent CSRF attacks
+    params = {"client_id": CLIENT_ID,
+              "response_type": "code",
+              "state": state,
+              "scope": scope}
+    url = "https://quizlet.com/authorize?" + parse.urlencode(params)
+
+    # convoluted return statement to return a named tuple, for better readability
+    # state is also returned in order to verify with the state Quizlet's API returns
+    return url
+
+def _get_access_token(authorization_code):
+    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    post_data = {"grant_type": "authorization_code",
+                 "code": authorization_code,
+                 "redirect_uri": "http://localhost:8000"}
+
+    response = requests.post("https://api.quizlet.com/oauth/token", auth=client_auth, data=post_data)
+    token_json = response.json()
+    
+    return token_json
