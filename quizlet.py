872feb0002cafb85acbc778023efafd3b80de720
Ridha Hassoun <ridha.hassoun@gmail.com>
2016-12-22 19:25:09 -0800

    Added class method to create session from access token. Added comments.

--- quizlet.py
+++ quizlet.py
@@ -31,23 +31,40 @@ class QuizletSession():
         if invalid_scopes:
             raise errors.ScopeError(invalid_scopes)
 
+        # Prepare authorization URL for OAuth2 flow
         url = self._make_authorization_url(self._client_id, state, scope)
         webbrowser.open_new(url)
 
+        # Set up local server to receive redirect and handle it
         server = HTTPServer(('', 8000), _QuizletAuthorizationHTTPHandler)
         server.data = (self._client_id, self._client_secret, state)
         server.handle_request()
 
+        # After handle_request() completes, retrieve access token
         access_token_json = server.access_token
-        self._user_id = access_token_json["user_id"]
-        self._access_token = access_token_json["access_token"]
+        self.user_id = access_token_json["user_id"]
+        self.access_token = access_token_json["access_token"]
+        self.expiration = access_token_json["expires_in"]
+
+    @classmethod
+    def using_access_token(cls, user_id, access_token):
+        """Creates a new session without going through OAuth2 flow.
+        Requires both user's user ID and access token."""
+
+        session = super().__new__(cls)
+        session.user_id = user_id
+        session.access_token = access_token
+        return session
 
     def get_current_user(self):
+        """Returns the current authenticated user."""
+
         header = {
             "Authorization": "Bearer {}".format(self.access_token)
         }
         api_url = "https://api.quizlet.com/2.0/users/{}".format(self.user_id)
 
+        # making a GET request for current user
         qzlt_request = request.Request(api_url, headers=header)
         response = request.urlopen(qzlt_request)
         user_json = json.loads(response.read().decode("UTF-8"))
@@ -65,23 +82,6 @@ class QuizletSession():
         url = "https://quizlet.com/authorize?" + parse.urlencode(params)
 
         return url
-
-    # Properties
-    @property
-    def client_id(self):
-        return self._client_id
-
-    @property
-    def client_secret(self):
-        return self._client_secret
-
-    @property
-    def user_id(self):
-        return self._user_id
-
-    @property
-    def access_token(self):
-        return self._access_token
 
 
 class _QuizletAuthorizationHTTPHandler(BaseHTTPRequestHandler):
@@ -119,6 +119,10 @@ class _QuizletAuthorizationHTTPHandler(BaseHTTPRequestHandler):
         return
 
     def _get_access_token(self, client_id, client_secret, authorization_code):
+        """This function sets up POST request for access token and then requests
+        it. It returns access token provided by Quizlet after OAuth2 flow
+        has completed."""
+
         base64string = base64.b64encode(
             bytes("{}:{}".format(client_id, client_secret), "UTF-8"))
         header = {
@@ -131,6 +135,7 @@ class _QuizletAuthorizationHTTPHandler(BaseHTTPRequestHandler):
         }
         data = parse.urlencode(post_data).encode("UTF-8")
 
+        # making a POST request to get access token
         qzlt_request = request.Request("https://api.quizlet.com/oauth/token",
                                        data=data,
                                        headers=header)
--- users.py
+++ users.py
@@ -10,13 +10,13 @@ class User():
         if json:
             self._raw_json = json
 
-            self._username = json["username"]
-            self._account_type = json["account_type"]
-            self._id_number = json["id"]
-            self._signup_date = datetime.fromtimestamp(json["sign_up_date"])
-            self._profile_picture = json["profile_image"]
-            self._statistics = json["statistics"]
-            self._sets = json["sets"]
-            self._favorite_sets = json["favorite_sets"]
-            self._studied_sets = json["studied"]
-            self._groups = json["groups"]
+            self.username = json["username"]
+            self.account_type = json["account_type"]
+            self.id_number = json["id"]
+            self.signup_date = datetime.fromtimestamp(json["sign_up_date"])
+            self.profile_picture = json["profile_image"]
+            self.statistics = json["statistics"]
+            self.sets = json["sets"]
+            self.favorite_sets = json["favorite_sets"]
+            self.studied_sets = json["studied"]
+            self.classes = json["groups"]
